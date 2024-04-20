89321eeb3c3688ef5337e20f3b7f70e3cfecff3c
paoced <165980300+paoced@users.noreply.github.com>
2024-04-19 21:47:49 -0600

    Update app.py
    se agregó el segundo boton

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
+disp_button = st.button('Construir diagrama de dispersion')
         
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
@@ -11,6 +12,17 @@ if hist_button: # al hacer clic en el botón
             
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+#Creando el 2do boton para hacer un grafico de dispersion
+if disp_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un diagrama de dispersion
+    fig = px.scatter(car_data, x="odometer", y="price")
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
